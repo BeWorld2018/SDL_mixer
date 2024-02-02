708bec4b4e63521c0e014a2b1d72bf92b5681f00
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2024-02-02 11:13:02 +0100

    MorphOS: update version

--- MorphOS/MIX_version.h
+++ MorphOS/MIX_version.h
@@ -1,5 +1,5 @@
 #define	str(s) #s
 #define	xstr(s) str(s)
 #define	VERSION	53
-#define	REVISION	5
+#define	REVISION	6
 #define	VERSTAG	"\0$VER: sdl2_mixer.library " xstr(VERSION) "." xstr(REVISION) " (" __AMIGADATE__ ") (c) Bruno Peloille, Szilard Biro"
